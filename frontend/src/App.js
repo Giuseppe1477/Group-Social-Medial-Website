465cc682c396b1edcdcf3950b6e6edacdb785a4e
ronnathaniel <ronnathaniel>
2021-09-26 19:05:18 -0400

    fix(App + index): Created logged_out state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import InputBox from './InputBox.js';
+import HomePage from './HomePage.js';
+
+
+const App = () => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    return (
+        <div className="App">
+
+            {isLoggedIn ?
+                <HomePage
+                    isAdmin={isAdmin}
+                />
+            :
+                <InputBox
+                    login={() => {
+                        console.log('Logging In.');
+                        setTimeout(() => setIsLoggedIn(true), 0);
+                    }}
+                    setAdmin={setIsAdmin}
+                />
+            }
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,63 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
 
-class InputBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      loggedIn: false,
-      username: "",
-      password: "",
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+import App from './App.js';
 
-  handleChange (event) {
-    let name = event.target.name;
-    let value = event.target.value;
-    this.setState({[name]:value});
-  }
-
-  handleSubmit (event) {
-    event.preventDefault();
-    alert("You are submitting " + this.state.username + " " + this.state.password);
-    fetch('https://4hduy444e6.execute-api.us-east-1.amazonaws.com/auth',{
-      method: 'POST',
-      mode: 'cors',
-      cache: 'force-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-        user: this.state.username,
-        pass: this.state.password,
-      })
-    })
-    .then(response=>response.json())
-    .then(data=>console.log(data));
-  }
-
-  render(){
-    return(
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Username:
-          <input name="username" type="text" onChange={this.handleChange}/>
-        </label>
-        <br/>
-        <label>
-          Password:
-          <input name="password" type="text" onChange={this.handleChange}/>
-        </label>
-        <br/>
-        <button>Login</button>
-      </form>
-    )
-  }
-}
-
-ReactDOM.render(<InputBox />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
