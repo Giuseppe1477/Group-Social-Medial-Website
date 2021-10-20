712a683d9f2c7282efb9559e91f9a1e8abcb78e4
ronnathaniel <rnathaniel7@gmail.com>
2021-10-20 16:39:10 -0400

    fix(ListPosts): created as map of post lists

--- frontend/src/ListPosts.js
+++ frontend/src/ListPosts.js
@@ -1,11 +1,18 @@
 import Post from './Post.js';
 
-const ListPosts = ({posts}) => {
+const ListPosts = ({user_id, posts}) => {
     return ( 
         <div>
-            {posts.map((post) => (
-                <Post post_id={post.posts.post_id}/>
-            ))}
+            {posts.map(
+                (p, idx) => <Post
+                    user_id={user_id}
+                    key={idx}
+                    user_poster_id={p.user_id}
+                    post_id={p.post_id}
+                    text={p.text}
+                    created_at={p.created_at}
+                />
+            )}
         </div>
      );
 }
